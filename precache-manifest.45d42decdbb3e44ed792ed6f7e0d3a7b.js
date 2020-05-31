4b490b2255b11c32776c0bb5fec0bb096eb284a1
AlexRozv <AlexRozv@gmail.com>
2020-05-31 12:42:03 +0300

    Updates

---
+++ precache-manifest.45d42decdbb3e44ed792ed6f7e0d3a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675df94227b80f5ceda2e4aed3c5dff1",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "104d902a972fc54a1f40",
+    "url": "/weather/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "406a6b0186af23861fa5",
+    "url": "/weather/static/js/2.4df85e87.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather/static/js/2.4df85e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104d902a972fc54a1f40",
+    "url": "/weather/static/js/main.6bba9d7b.chunk.js"
+  },
+  {
+    "revision": "9c21fed636a91d06e10c",
+    "url": "/weather/static/js/runtime-main.8d44f662.js"
+  }
+]);
